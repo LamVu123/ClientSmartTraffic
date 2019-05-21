3911678c2267f9e63ccb955c2d5c3dff0eb2b48e
lamvu123 <lamvu.minsk89@gmail.com>
2019-05-21 13:54:26 +0700

    edit structure Folder

---
+++ Source/Client/DemoMapsAPI/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -0,0 +1,300 @@
+ package com.example.myapplication;
+
+ import android.Manifest;
+ import android.app.AlertDialog;
+ import android.app.ProgressDialog;
+ import android.content.DialogInterface;
+ import android.content.Intent;
+ import android.content.pm.PackageManager;
+ import android.graphics.Color;
+ import android.location.Location;
+ import android.location.LocationListener;
+ import android.location.LocationManager;
+ import android.os.Build;
+ import android.os.Bundle;
+ import android.os.Process;
+ import android.provider.Settings;
+ import android.support.annotation.NonNull;
+ import android.support.v4.app.ActivityCompat;
+ import android.support.v4.app.FragmentActivity;
+ import android.util.Log;
+ import android.view.View;
+ import android.widget.Button;
+ import android.widget.EditText;
+ import android.widget.TextView;
+ import android.widget.Toast;
+
+ import com.google.android.gms.maps.CameraUpdate;
+ import com.google.android.gms.maps.CameraUpdateFactory;
+ import com.google.android.gms.maps.GoogleMap;
+ import com.google.android.gms.maps.OnMapReadyCallback;
+ import com.google.android.gms.maps.SupportMapFragment;
+ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+ import com.google.android.gms.maps.model.LatLng;
+ import com.google.android.gms.maps.model.Marker;
+ import com.google.android.gms.maps.model.MarkerOptions;
+ import com.google.android.gms.maps.model.Polyline;
+ import com.google.android.gms.maps.model.PolylineOptions;
+
+ import java.io.UnsupportedEncodingException;
+ import java.util.ArrayList;
+ import java.util.List;
+
+ import Modules.*;
+
+ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,DirectionFinderListener {
+
+    private GoogleMap mMap;
+    private static final int REQUEST_CODE = 0;
+    private static final String TAG = "MapsActivity";
+    public static LatLng currentLocation;
+     private Button btnFindPath;
+     private Button btnAcc;
+     private EditText etOrigin;
+     private EditText etDestination;
+     private List<Marker> originMarkers = new ArrayList<>();
+     private List<Marker> destinationMarkers = new ArrayList<>();
+     private List<Polyline> polylinePaths = new ArrayList<>();
+     private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        turnOnGPS();
+        if (Build.VERSION.SDK_INT >= 23) {
+            setPermission();
+        } else {
+            getCurrentLocation();
+        }
+
+        btnFindPath = (Button) findViewById(R.id.btnFindPath);
+        btnAcc = (Button) findViewById(R.id.btnAcc);
+        etOrigin = (EditText) findViewById(R.id.etOrigin);
+        etDestination = (EditText) findViewById(R.id.etDestination);
+
+        btnFindPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+
+        btnAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MapsActivity.this);
+                alert.setTitle("Smart Traffic");
+                alert.setMessage("Hiện thị đồ thị realtime - ku Trí");
+                alert.setPositiveButton("OK",null);
+                alert.show();
+            }
+        });
+    }
+
+    //set permission to access location and getCurrentLocation
+    private void setPermission() {
+        //check and request permission
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+                && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}
+                    , REQUEST_CODE);
+        } else {
+            getCurrentLocation();
+        }
+    }
+
+    //user accept permission to access location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        getCurrentLocation();
+    }
+
+    // Check gps, if turn off => turn on request.
+    private void turnOnGPS() {
+        //get gps status
+        String provider = Settings.Secure.
+                getString(getContentResolver(),
+                        Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if (!provider.contains("gps")) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Smart Traffic");
+            builder.setMessage("Do you turn on your GPS?");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Toast.makeText(MainActivity.this, "mess", Toast.LENGTH_SHORT).show();
+                    startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                            , 0);
+                }
+            });
+            builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //dialogInterface.dismiss();
+                    Process.killProcess(Process.myPid());
+                    System.exit(1);
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+
+    private void getCurrentLocation() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                //Log.d(TAG, "onLocateChange: "+ currentLocation);
+                Toast.makeText(MapsActivity.this, "kinh độ - vĩ độ: " + currentLocation.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+                && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        ) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                3000, 0,
+                locationListener);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng fptUniLocal = new LatLng(21.013138, 105.526876);
+        mMap.addMarker(new MarkerOptions().position(fptUniLocal).title("Marker in FPT University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fptUniLocal,15));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        if(currentLocation !=null) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLocation,15);
+            mMap.animateCamera(cameraUpdate);
+        }
+    }
+
+    private void sendRequest() {
+        String origin = etOrigin.getText().toString();
+        String destination = etDestination.getText().toString();
+        if (origin.isEmpty()) {
+            Toast.makeText(this, "Please enter origin address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (destination.isEmpty()) {
+            Toast.makeText(this, "Please enter destination address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Log.d(TAG,"etOrigin: "+origin);
+        //Log.d(TAG,"etDestination: "+destination);
+
+        try {
+            new DirectionFinder(this, origin, destination).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDirectionFinderStart() {
+        progressDialog = ProgressDialog.show(this, "Please wait.",
+                "Finding direction..!", true);
+
+        if (originMarkers != null) {
+            for (Marker marker : originMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (destinationMarkers != null) {
+            for (Marker marker : destinationMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (polylinePaths != null) {
+            for (Polyline polyline:polylinePaths ) {
+                polyline.remove();
+            }
+        }
+    }
+
+     @Override
+     public void onDirectionFinderSuccess(List<Route> routes) {
+         progressDialog.dismiss();
+         polylinePaths = new ArrayList<>();
+         originMarkers = new ArrayList<>();
+         destinationMarkers = new ArrayList<>();
+
+         for (Route route : routes) {
+             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 15));
+             ((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
+             ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);
+
+             originMarkers.add(mMap.addMarker(new MarkerOptions()
+                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.start_blue))
+                     .title(route.startAddress)
+                     .position(route.startLocation)));
+             destinationMarkers.add(mMap.addMarker(new MarkerOptions()
+                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.end_green))
+                     .title(route.endAddress)
+                     .position(route.endLocation)));
+
+             PolylineOptions polylineOptions = new PolylineOptions().
+                     geodesic(true).
+                     color(Color.BLUE).
+                     width(10);
+
+             for (int i = 0; i < route.points.size(); i++)
+                 polylineOptions.add(route.points.get(i));
+
+             polylinePaths.add(mMap.addPolyline(polylineOptions));
+         }
+     }
+}
